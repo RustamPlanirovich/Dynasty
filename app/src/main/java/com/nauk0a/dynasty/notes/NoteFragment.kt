cd1c7e7a01880865b72efc6f2604bd6e0acd4f62
RustamPlanirovich <israr.ru@yandex.ru>
2021-12-19 21:33:35 +0700

    - refactoring - сортировка получаемого списка по дате добавления.

--- app/src/main/java/com/nauk0a/dynasty/notes/NoteFragment.kt
+++ app/src/main/java/com/nauk0a/dynasty/notes/NoteFragment.kt
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
@@ -16,7 +19,7 @@ import com.nauk0a.dynasty.R
 import com.nauk0a.dynasty.databinding.NoteFragmentBinding
 import com.nauk0a.dynasty.notes.adapter.NotesAdapter
 import com.nauk0a.dynasty.utils.db
-import com.google.firebase.firestore.DocumentSnapshot
+import java.util.*
 
 
 class NoteFragment : Fragment(), NotesAdapter.NotesAdapterListener {
@@ -27,6 +30,8 @@ class NoteFragment : Fragment(), NotesAdapter.NotesAdapterListener {
     private val binding get() = _binding!!
     private lateinit var adapter: NotesAdapter
     private lateinit var query: Query
+
+    private lateinit var database: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,13 +45,16 @@ class NoteFragment : Fragment(), NotesAdapter.NotesAdapterListener {
         super.onViewCreated(view, savedInstanceState)
 
         db = Firebase.firestore
+        database = Firebase.database.reference
+
+        query = FirebaseFirestore.getInstance().collection("notes").orderBy("date", Query.Direction.DESCENDING)
+//        query.orderBy("date", Query.Direction.DESCENDING)
 
 
 
-        query = FirebaseFirestore.getInstance().collection("notes")
 
 
-        adapter = NotesAdapter(query,this)
+        adapter = NotesAdapter(query, this)
         binding.notesListRv.setHasFixedSize(true)
 //        binding.notesListRv.itemAnimator = null
         binding.notesListRv.adapter = adapter
@@ -71,7 +79,6 @@ class NoteFragment : Fragment(), NotesAdapter.NotesAdapterListener {
 //            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully deleted!") }
 //            .addOnFailureListener { e -> Log.w(TAG, "Error deleting document", e) }
     }
-
 
 
     override fun onStart() {
